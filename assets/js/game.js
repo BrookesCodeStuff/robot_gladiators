29b1c831a0f619f95134be8b01a02f21ba76ee8d
Brooke <brookepaglia@gmail.com>
2021-12-25 12:30:30 -0500

    add first round of fight logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,58 @@
-// Fight function
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-  window.alert('The fight has begun!');
-}
+var enemyName = 'Roborto';
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+  //Alert players that they are starting the round
+  window.alert('Welcome to Robot Gladiators!');
+
+  // Subtract value of playerAttack from value of enemyHealth
+  enemyHealth = enemyHealth - playerAttack;
+
+  // Log resulting message to console
+  console.log(
+    playerName +
+      ' attacked ' +
+      enemyName +
+      '. ' +
+      enemyName +
+      ' now has ' +
+      enemyHealth +
+      ' health remaining!'
+  );
+
+  // Check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + ' has died!');
+  } else {
+    window.alert(enemyName + ' still has ' + enemyHealth + ' health left!');
+  }
+
+  // Subtract value of enemyAttack from value of playerHealth
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log resulting message to console
+  console.log(
+    enemyName +
+      ' attacked ' +
+      playerName +
+      '. ' +
+      playerName +
+      ' now has ' +
+      playerHealth +
+      ' health remaining!'
+  );
+
+  // Check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + ' has died!');
+  } else {
+    window.alert(playerName + ' still has ' + playerHealth + ' health left!');
+  }
+};
 
 fight();
