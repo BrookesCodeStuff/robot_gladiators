9dceba41beffb5bdc108cc93a5999367a31a7974
Brooke <brookepaglia@gmail.com>
2021-12-24 22:59:39 -0500

    add ability to get player name and start fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-alert("JavaScript is running");
+// Fight function
+var playerName = window.prompt("What is your robot's name?");
+
+function fight() {
+  window.alert('The fight has begun!');
+}
+
+fight();
